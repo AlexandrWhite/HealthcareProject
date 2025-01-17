cc51cc91d196d7679f4aad3a3d91a7f8d65ccc3f
Alex <baa@mail.osu.ru>
2025-01-17 17:22:48 +0500

    Сделал окончательную базу для страницы пациента

--- frontend/src/components/PatientPage/PatientPage.tsx
+++ frontend/src/components/PatientPage/PatientPage.tsx
@@ -1,11 +1,11 @@
-import { ThemeProvider } from "@gravity-ui/uikit";
+import { Button, Select, TextArea, TextInput, ThemeProvider } from "@gravity-ui/uikit";
 import { useEffect, useState } from "react";
 import {useParams,useNavigate} from 'react-router-dom'
 import {Spin} from '@gravity-ui/uikit';
 import {DisplayPulse} from '@gravity-ui/icons';
 
 import axios from "axios";
-import { AsideHeader, Settings } from "@gravity-ui/navigation";
+import { AsideFallback, AsideHeader, PageLayout, Settings } from "@gravity-ui/navigation";
 const serverUrl = 'http://localhost:8000/'
 
 
@@ -53,46 +53,170 @@ export const PatientPage: React.FC = () =>{
         )
     }
 
+    const formatPhoneNumber = (number) => {
+        const cleaned = ('' + number).replace(/\D/g, '');
+
+        // Проверяем, что номер состоит из 10 цифр
+        const match = cleaned.match(/^7?(\d{3})(\d{3})(\d{4})$/);
+    
+        if (match) {
+            // Если номер начинается с 7, добавляем её отдельно
+            return `+7 (${match[1]}) ${match[2]}-${match[3]}`;
+        }
+    };
+
     return (
         <ThemeProvider theme="light">
-            {/* <Settings filterPlaceholder="Найти параметр" 
-            title={curPatient['last_name']+" "+curPatient['first_name'] + " "+curPatient['patronym']}>
-                <Settings.Group groupTitle="Test">
-                    <Settings.Page title="MyTest">
-                        ...
-                    </Settings.Page>
-                </Settings.Group>
-            </Settings> */}
-            <AsideHeader compact={false} hideCollapseButton={true}
-                logo={{
-                    text:curPatient['last_name']+" "+curPatient['first_name'] + " "+curPatient['patronym']
-                }}
-                menuItems={[
-                    {
-                        id:"infoPatient",
-                        title:"Информация о пациенте",
-                    },
-                    {
-                        id:"vitalParams",
-                        title:"Витальные параметры",
-                    },
-                    {
-                        id: 'divider',
-                        title: '-',
-                        type: 'divider',
-                    },
-                    {
-                        id:"diagnos",
-                        title:"Поставить диагноз",
-                        type:'action',
-                        icon:DisplayPulse,
-                        onItemClick(){
-                            alert("Куда жмешь?")
+                {/* <AsideHeader compact={false} hideCollapseButton={true}
+                    logo={{
+                        text:curPatient['last_name']+" "+curPatient['first_name'] + " "+curPatient['patronym']
+                    }}
+                    
+                    renderContent={(event) => {
+                        return(
+
+                            <h1>{JSON.stringify(event)}</h1>
+                        )
+                    }}
+
+                    menuItems={[
+                        {
+                            id:"infoPatient",
+                            title:"Информация о пациенте",
+                            onItemClick(){
+                                alert("Test")
+                            }
+                            
+                        },
+                        {
+                            id:"vitalParams",
+                            title:"Витальные параметры",
+                        },
+                        {
+                            id: 'divider',
+                            title: '-',
+                            type: 'divider',
+                        },
+                        {
+                            id:"diagnos",
+                            title:"Поставить диагноз",
+                            type:'action',
+                            icon:DisplayPulse,
+                            onItemClick(){
+                                alert("Куда жмешь?")
+                            }
                         }
-                    }
-                ]}
-            >
-            </AsideHeader>
+                    ]}
+                >
+                
+                </AsideHeader> */}
+
+
+
+                <Settings title={curPatient['last_name']+" "+curPatient['first_name'] + " "+curPatient['patronym']} filterPlaceholder="Найти параметр" 
+                emptyPlaceholder="Ничего">
+                    <Settings.Group id="main" groupTitle="Параметры пациента">
+
+                        <Settings.Page title="Информация о пациенте">
+                            <Settings.Section title={curPatient['last_name']+" "+curPatient['first_name'] + " "+curPatient['patronym']}>
+                                <Settings.Item title="Дата рождения">
+                                    <p>{curPatient['birth_date']}</p>
+                                </Settings.Item>
+                                <Settings.Item title="Пол">
+                                    <p>{curPatient['gender']}</p>
+                                </Settings.Item>
+                                <Settings.Item title="Социальный статус">
+                                    <p>{curPatient['social_status']}</p>
+                                </Settings.Item>
+                                <Settings.Item title="Адрес проживания">
+                                    <p>{curPatient['living_adress']}</p>
+                                </Settings.Item>
+                                <Settings.Item title="Адрес регистрации">
+                                    <p>{curPatient['registration_adress']}</p>
+                                </Settings.Item>
+                                <Settings.Item title="Телефон">
+                                    <p>
+                                    {
+                                        formatPhoneNumber(curPatient['phone_number'])
+                                    }
+                                    </p>
+                                </Settings.Item>
+                                <Settings.Item title="Номер страхового полиса">
+                                    <p>{curPatient['insurance_number']}</p>
+                                </Settings.Item>
+                            </Settings.Section>
+                        </Settings.Page>
+                        
+
+                        <Settings.Page title="Витальные параметры">
+                                <Settings.Section title="">
+                                    <Settings.Item title="Рост">
+                                        <TextInput />
+                                    </Settings.Item>
+
+                                    <Settings.Item title="Вес">
+                                        <TextInput />
+                                    </Settings.Item>
+                                    
+                                    <Settings.Item title="Температура">
+                                        <TextInput />
+                                    </Settings.Item>
+                                </Settings.Section>
+                        </Settings.Page>
+
+                    </Settings.Group>
+
+                    <Settings.Group groupTitle="Опрос пациента">
+                        <Settings.Page title="Опрос">
+                            <Settings.Section title="">
+                                <Settings.Item title="Наличие кровотечения">
+                                    <Select width={170}>
+                                        <Select.Option value="yes">Да</Select.Option>
+                                        <Select.Option value="no">Нет</Select.Option>
+                                    </Select>
+                                </Settings.Item>
+
+                                <Settings.Item title="Наличие на момент обследования восполительных заболеваний, онкологий">
+                                    <Select width={170}>
+                                        <Select.Option value="yes">Да</Select.Option>
+                                        <Select.Option value="no">Нет</Select.Option>
+                                    </Select>
+                                </Settings.Item>
+
+                                <Settings.Item title="Инфекции, переливание крови, отравление, интоксикация">
+                                    <Select width={170}>
+                                        <Select.Option value="yes">Да</Select.Option>
+                                        <Select.Option value="no">Нет</Select.Option>
+                                    </Select>
+                                </Settings.Item>
+
+                                <Settings.Item title="Результат ультразвукового исследования">
+                                    <Select width={170}>
+                                        <Select.Option value="yes">Отклонения есть</Select.Option>
+                                        <Select.Option value="no">Отклонениий нет</Select.Option>
+                                    </Select>
+                                </Settings.Item>
+                                
+                                <Settings.Item>
+                                    <Button view="outlined-success" children="Сохранить" size="l"/>
+                                </Settings.Item>
+
+                            </Settings.Section>
+                        </Settings.Page>
+
+                        <Settings title="Осмотр">
+                            <Settings.Item title="">
+                                <TextArea minRows={20}/>
+                            </Settings.Item>
+                            <Settings.Item title="">
+                            <Button view="outlined-success" children="Сохранить" size="l"/>
+                            </Settings.Item>
+                        </Settings>
+                    </Settings.Group>
+
+                    
+
+                </Settings>
         </ThemeProvider>
     )
 }
