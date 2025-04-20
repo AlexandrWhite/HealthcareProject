27c5cd6bf64b817edaa3e8b9be36ce595f7cd411
Alexandr Belyy <sashabelyy123@gmail.com>
2025-04-20 18:41:45 +0500

    Добавил модель Visit

--- backend/healthcare/admin.py
+++ backend/healthcare/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Patient
+from .models import Visit
 
 # Register your models here.
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('first_name','last_name', 'patronym', 'birth_date')
 
 admin.site.register(Patient, PatientAdmin)
+
+class VisitAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Visit._meta.fields]
+admin.site.register(Visit, VisitAdmin)
--- backend/healthcare/models.py
+++ backend/healthcare/models.py
@@ -15,3 +15,13 @@ class Patient(models.Model):
     social_status = models.CharField(max_length=120, default='работает')
     insurance_number = models.CharField(max_length=14,default='000-000-000 00')
     phone_number = models.CharField(max_length=11, default='74957556983')
+
+class Visit(models.Model):
+    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='visits')
+    tapID = models.IntegerField() 
+    investigationName = models.CharField(max_length=255)
+    investigationResult = models.TextField()
+    investigationDate = models.DateField()
+
+    def __str__(self):
+        return f"Visit of {self.patientID} on {self.investigationDate}"
